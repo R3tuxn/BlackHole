1806b7683f0b08f9008bb5d9a08a1adc0886c448
R3tuxn <35817110+R3tuxn@users.noreply.github.com>
2018-04-28 16:55:11 +0200

    Update SECRETS.java

--- src/main/java/util/SECRETS.java
+++ src/main/java/util/SECRETS.java
@@ -2,6 +2,6 @@ package util;
 
 public class SECRETS {
 
-    public static final String TOKEN = "NDE0NzUwMzA2OTYyODk4OTQ1.DWr53A.zZEnP_mzuR7ttA_1YtNTzPtoW28";
+    public static final String TOKEN = "";
 
 }
