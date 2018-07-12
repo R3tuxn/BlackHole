a9ad95ffb8287878944070ff6e5c765e901de24c
R3tuxn <35817110+R3tuxn@users.noreply.github.com>
2018-07-13 00:27:58 +0200

    Test

--- src/main/java/listeners/JoinListener.java
+++ src/main/java/listeners/JoinListener.java
@@ -16,40 +16,9 @@ public class JoinListener extends ListenerAdapter{
 
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
 
-        EmbedBuilder em1 = new EmbedBuilder();
-        String ID1 = event.getGuild().getMemberById("285480119575904256").getUser().getDiscriminator();
-        String ID2 = event.getGuild().getMemberById("306782515040092171").getUser().getDiscriminator();
-        String CoOwner = event.getGuild().getMemberById("285480119575904256").getUser().getName() + "#" + ID1;
-        String Owner = event.getGuild().getMemberById("306782515040092171").getUser().getName() + "#" + ID2;
+            EmbedBuilder builder = new EmbedBuilder();
 
-        if (STATIC.Switch1.equals("off")) {
-            event.getGuild().getTextChannelsByName("welcome", true).get(0).sendMessage(em1.setDescription("Bot disabled!").setFooter("Please contact " + CoOwner + " or " + Owner + "!", null).setColor(Color.red).build()).queue();
-            return;
-        }
+        event.getGuild().getTextChannelsByName("welcome", true).get(0).sendMessage(builder.setColor(Color.green).setDescription("Welcome **" + event.getMember().getUser().getName() + "** at Black Hole!\nYou are the **" + event.getGuild().getMembers().size() + "** Member! :tada:\n\nHave a lot fun on our Server!").setTimestamp(Instant.now()).build()).queue();
 
-
-        EmbedBuilder builder = new EmbedBuilder();
-
-        try {
-            event.getGuild().getController().addRolesToMember(event.getMember(), event.getGuild().getRolesByName("[MEMBERS]", true).get(0)).queue();
-        } catch (Exception e) {
-            event.getGuild().getTextChannelsByName("welcome", true).get(1).sendMessage(builder.setDescription(":x: *Error* \n\nRole **[MEMBER]** was deleted or renamed !").setColor(Color.red).build()).queue();
-        }
-
-
-        event.getGuild().getTextChannelsByName("owner-lounge", true).get(1).sendMessage(builder.setColor(Color.green).setDescription("Welcome " + event.getMember().getUser().getAsMention() + " at Black Hole! You are the **" + event.getGuild().getMembers().size() + "** Member! :tada:\nHave a lot fun on our Server!").setTimestamp(Instant.now()).build()).queue();
-
-
-        /*MessageChannel channel = event.getGuild().getTextChannelsByName("welcome", true).get(0);
-
-        String NewMember = "Welcome " + event.getMember().getUser().getName() +"! :wave: ";
-        String Regeln = event.getGuild().getTextChannelsByName("rules", true).get(0).getAsMention();
-
-        RestAction<Message> action = channel.sendMessage(builder.setDescription("You are the " + "**" +
-                event.getGuild().getMembers().size() + "**" +" Member! :tada: \n\nPlease read the " + Regeln + "! You can register your account by clicking the hook!")
-                .setTitle(NewMember, null).setColor(Color.green).build());
-        Message message = action.complete();
-        message.addReaction("✅").complete();
-*/
     }
 }
