69b8c6550cbb357c558d6c728812346227acb6f0
R3tuxn <35817110+R3tuxn@users.noreply.github.com>
2018-08-07 21:44:16 +0200

    Test

--- src/main/java/util/SECRETS.java
+++ src/main/java/util/SECRETS.java
@@ -2,6 +2,6 @@ package util;
 
 public class SECRETS {
 
-    public static final String TOKEN = "NDE0NzUwMzA2OTYyODk4OTQ1.DeokRw.1L0iY8mkkrG5iOe64i6CIVTK3NU";
+    public static final String TOKEN = "";
 
 }
